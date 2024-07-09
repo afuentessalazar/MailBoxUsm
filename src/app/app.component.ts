a12ab777ddd8639d6e672931eef8c82925f990d0
afuentess1 <130768840+afuentess1@users.noreply.github.com>
2024-07-09 15:38:08 -0400

    archivo listo para entrega

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { LoginComponent } from './login/login.component'; // Asegurarse de que el import es correcto
+import { LoginComponent } from './login/login.component'; 
 
 @Component({
-  selector: 'app-root', // Cambiar el selector a 'app-root'
+  selector: 'app-root', 
   standalone: true,
-  imports: [LoginComponent, RouterOutlet, RouterLink], // Importar LoginComponent
+  imports: [LoginComponent, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/redactar/redactar.component.ts
+++ src/app/redactar/redactar.component.ts
@@ -4,11 +4,7 @@ import { HomeComponent } from '../home/home.component';
 import { ComunicadosService } from '../services/comunicados.service'; 
 import { FormsModule, } from '@angular/forms'; 
 import { CommonModule } from '@angular/common';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Si aún no está importado
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 @Component({
